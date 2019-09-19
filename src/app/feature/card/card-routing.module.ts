6f07acad5947a690d0948841c8929cf9200e94af
Michael <mihendricks1@gmail.com>
2019-09-20 00:29:00 +0200

    feat: Created Card3 1. It's the flipping book card.

--- src/app/feature/card/card-routing.module.ts
+++ src/app/feature/card/card-routing.module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './card.component';
 import { Card1Component } from "./card1/card1.component"; 
 import { Card2Component } from "./card2/card2.component";
+import { Card3Component } from "./card3/card3.component";
 
 const routes: Routes = [
   { path: "", component: CardComponent },
   { path: "card1", component: Card1Component },
-  { path: "card2", component: Card2Component } 
+  { path: "card2", component: Card2Component },
+  { path: "card3", component: Card3Component } 
 ]
 
 @NgModule({
@@ -21,6 +23,6 @@ const routes: Routes = [
 })
 export class CardRoutingModule { 
   static components = [ 
-    Card1Component, CardComponent, Card2Component
+    Card1Component, CardComponent, Card2Component, Card3Component
   ];
 }
