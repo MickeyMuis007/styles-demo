74c5e5eecd938e762763488f112a797bce596fce
Michael <mihendricks1@gmail.com>
2019-09-20 08:43:11 +0200

    feat: Card Menu 1. Creating a card menu component.

--- src/app/feature/card/card-routing.module.ts
+++ src/app/feature/card/card-routing.module.ts
@@ -6,9 +6,11 @@ import { CardComponent } from './card.component';
 import { Card1Component } from "./card1/card1.component"; 
 import { Card2Component } from "./card2/card2.component";
 import { Card3Component } from "./card3/card3.component";
+import { CardMenuComponent } from './component/card-menu/card-menu.component';
 
 const routes: Routes = [
   { path: "", component: CardComponent },
+  { path: "card-menu", component: CardMenuComponent },
   { path: "card1", component: Card1Component },
   { path: "card2", component: Card2Component },
   { path: "card3", component: Card3Component } 
@@ -23,6 +25,6 @@ const routes: Routes = [
 })
 export class CardRoutingModule { 
   static components = [ 
-    Card1Component, CardComponent, Card2Component, Card3Component
+    Card1Component, CardComponent, Card2Component, Card3Component, CardMenuComponent
   ];
 }
