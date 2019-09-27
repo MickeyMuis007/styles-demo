6fc2847082290ce387b2f1175f441b0fffd4821c
Michael <mihendricks1@gmail.com>
2019-09-27 21:36:40 +0200

    feat: Card Menu Item Styling 1 Updated card menu item style. 2 Removed unused card

--- src/app/feature/card/component/card-menu/card-menu.component.ts
+++ src/app/feature/card/component/card-menu/card-menu.component.ts
@@ -12,10 +12,10 @@ export class CardMenuComponent implements OnInit {
 
   ngOnInit() {
     this.cardMenuItems = [
-      { id: "1", name: "Card 1", description: "This is a decription on card 1", uri: "card1" },
+      { id: "1", name: "Card 1", description: `This is a decription on card 1,
+      This is a decription on card 1`, uri: "card1" },
       { id: "2", name: "Card 2", description: "This is a description on card 2", uri: "card2" },
       { id: "3", name: "Card 3", description: "This is a description on card 3", uri: "card3" },
-      { id: "4", name: "Card 3", description: "This is a description on card 3", uri: "card4" }
     ]
   }
 }
